cc3acc03755467a7f0d85cd6e8bae315081f2682
rahul <rahul5.up55@gmail.com>
2024-05-23 21:19:52 +0530

    eslint test

--- nextjsapp1/.eslintrc.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  extends: [
-    'plugin:@typescript-eslint/recommended',
-    'prettier/@typescript-eslint',
-    // Other configurations...
-  ],
-  // Other ESLint configurations...
-};
